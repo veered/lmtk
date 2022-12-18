26243237fedde795328602d806938d2b26bed767
Lucas Hansen <lucashansen@gmail.com>
2022-12-18 11:18:41 -0600

    update package description

--- gpt_repl/__init__.py
+++ gpt_repl/__init__.py
@@ -1,5 +1,5 @@
-"""Command line REPL for ChatGPT"""
-__version__ = '0.4.13'
+"""Command line REPL for GPT-based tools"""
+__version__ = '0.4.14'
 
 from .repl import REPL
 from .config import Config
