4858d59a75e00cdf2dead540961879198918e93d
Lucas Hansen <lucashansen@gmail.com>
2022-12-21 15:40:41 -0600

    remove dead code

--- gpt_repl/config.py
+++ gpt_repl/config.py
@@ -41,66 +41,6 @@ class Config:
   def threads(self):
     return ThreadManager(self)
 
-  def normalize_thread_id(self, thread_id):
-    if thread_id == None:
-      return None
-    thread_id = thread_id.replace('@', '').strip()
-    return re.sub(r'[^_a-zA-Z0-9\.]+', '_', thread_id)
-
-  def get_thread_file_path(self, thread_id):
-    thread_id = self.normalize_thread_id(thread_id)
-    thread_file_path = path.join(self.threads_dir_path, f"{thread_id}.json")
-    return thread_file_path
-
-  def load_thread(self, thread_id):
-    thread_id = self.normalize_thread_id(thread_id)
-
-    if thread_id == None:
-      return self.get_empty_thread(None)
-
-    thread_file_path = self.get_thread_file_path(thread_id)
-    if not path.exists(thread_file_path):
-      return self.get_empty_thread(thread_id)
-
-    with open(thread_file_path, "r") as thread_file:
-      thread_data = json.load(thread_file)
-
-    if not thread_data['state']:
-      thread_data['state'] = {}
-
-    return thread_data
-
-  def save_thread(self, thread_id, thread_data):
-    thread_id = self.normalize_thread_id(thread_id)
-    thread_file_path = self.get_thread_file_path(thread_id)
-    with open(thread_file_path, "w") as thread_file:
-      json.dump(thread_data, thread_file, indent=2)
-
-  def get_empty_thread(self, thread_id):
-    thread_id = self.normalize_thread_id(thread_id)
-    return {
-      "id": thread_id,
-      "mode": "",
-      'state': {},
-      "history": [],
-    }
-
-  # thanks gpt
-  def list_threads(self):
-    threads = []
-    for thread_file in listdir(self.threads_dir_path):
-      if thread_file.endswith(".json"):
-        thread_id = thread_file[:-5]  # remove the ".json" extension
-        thread_id = thread_id.replace("_", "-")  # replace underscores with dashes
-        thread_path = path.join(self.threads_dir_path, thread_file)
-        modification_time = path.getmtime(thread_path)
-        threads.append((modification_time, thread_id))
-
-    # sort threads by modification time in descending order
-    threads.sort(reverse=True)
-
-    return [thread_id for _, thread_id in threads]
-
   def load_plugins(self):
     files = os.listdir(self.plugins_dir_path)
     for file in files:
