1364da1681c40b2718fa4f15be1ecb634c88143a
Lucas Hansen <lucashansen@gmail.com>
2022-12-31 09:07:36 -0600

    rename script helper

--- gpt_repl/script/helpers.py
+++ gpt_repl/script/helpers.py
@@ -14,7 +14,7 @@ def get_file(file_path):
   with open(expand_path(file_path), 'r') as file:
     return file.read()
 
-def code_block(file_name):
+def show_code(file_name):
   file_path = expand_path(file_name)
   if not os.path.exists(file_path):
     raise FileNotFoundError(f'File "{file_name}" does not exist')
